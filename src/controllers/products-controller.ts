ff64ce8e6cf44ec8d713db4866fe1522cff4d068
MarlonChi <marlonchiodelli@hotmail.com>
2025-02-17 21:03:14 -0300

    feat: create product validation

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import z from "zod";
 
 export class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -6,6 +7,23 @@ export class ProductController {
       return response.json({ message: "ok" });
     } catch (err) {
       next(err);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z
+          .number()
+          .gt(0, { message: "O valor precisa ser maior do que 0" }),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      return response.status(201).json({ name, price });
+    } catch (error) {
+      next(error);
     }
   }
 }
