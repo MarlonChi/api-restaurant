749325f153ff3f349d3af2304c54ebc32ee0a744
MarlonChi <marlonchiodelli@hotmail.com>
2025-02-18 20:43:18 -0300

    feat: tables list

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
 import { productsRoutes } from "./products-routes";
+import { tablesRoutes } from "./tables-routes";
 
 export const routes = Router();
 
 routes.use("/products", productsRoutes);
+routes.use("/tables", tablesRoutes);
